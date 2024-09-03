3a5f5ef91808bf78a264c3f53a34de750997c329
Amir Hosseini <hosseeini.amir@gmail.com>
2024-09-03 21:05:48 +0200

    Try to fix CORS

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,25 @@ const nextConfig = {
             'stage-app-profiles-germany.s3.eu-central-1.amazonaws.com',
             'stage-app-profiles-germany.s3.amazonaws.com'
         ],
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: process.env.NODE_ENV === 'development'
+                            ? 'http://amirhs.test'
+                            : 'https://stage.amirhs.de'
+                    },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization' },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
