5b2fda3e0b0a0fa22002408133456f9768538c6b
Amir Hosseini <hosseeini.amir@gmail.com>
2024-09-15 18:29:41 +0200

    Work on image gallery of Profile

--- app/component/profiles/Profile.tsx
+++ app/component/profiles/Profile.tsx
@@ -2,12 +2,14 @@
 import * as React from 'react';
 import { ImageList, ImageListItem, Paper } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import { styled } from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import { PATHS } from "@/app/constants/paths";
 import Link from "next/link";
+import { useMediaQuery } from "@mui/system";
+import { useEffect, useState } from "react";
 
 
 interface Props {
@@ -32,34 +34,72 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function Profile({ profileId }: Props) {
+    const theme = useTheme();
+    const isXs = useMediaQuery(theme.breakpoints.down('xs'));
+    const isSm = useMediaQuery(theme.breakpoints.between('xs', 'sm'));
+    const isMd = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+    const isLg = useMediaQuery(theme.breakpoints.up('md'));
+    const [imageListHeight, setImageListHeight] = useState(0);
+
+    useEffect(() => {
+        let height = 0;
+        if (isXs) {
+            height = 250;
+        } else if (isSm) {
+            height = 330;
+        } else if (isMd) {
+            height = 550;
+        } else {
+            height = 750;
+        }
+        setImageListHeight(height); // Update state with the height
+    }, [isXs, isSm, isMd, isLg]);
+
+    // Set columns based on screen size
+    const getCols = () => {
+        if (isXs) return 1;
+        if (isSm) return 1;
+        if (isMd) return 2;
+        return 4;
+    };
+
+    const filteredItemData = isSm ? [{img: itemData[0].img, title: itemData[0].title, cols: 2, rows: 0}] : itemData;
+
     return (
         <>
             <ImageList
                 sx={{
                     position: 'relative',
                     left: 0,
+                    top: 50,
                     width: '100%',
-                    height: 650,
-                    overflowY: 'scroll',
+                    height: {xs: 250, sm: 450, md: 550, lg: 600},
+                    overflowY: 'hidden',
+                    overflowX: 'hidden',
+                    transition: '0.5s',
                     borderRadius: '8px',
-                    border: '1px solid rgba(255, 255, 255, 0.3)',
                     boxShadow: '0px 0px 30px -10px rgba(255, 255, 255, 0.2)',
                 }}
-                variant="quilted"
-                cols={5}
-                rowHeight={200}
+                cols={getCols()}
+                gap={0}
             >
-                {itemData.map((item) => (
-                    <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-                        <img
-                            {...srcset(item.img, 121, item.rows, item.cols)}
-                            alt={item.title}
-                            loading="lazy"
-                        />
+                {filteredItemData.map((item) => (
+                    <ImageListItem key={item.img}
+                                   cols={item.cols || 1}
+                                   rows={item.rows || 1}
+                    >
+                        <Link href='#' style={{ display: 'flex', margin: 0, padding: 0, height: '100%' }}>
+                            <img
+                                { ...srcset(item.img, 500, item.rows, item.cols) }
+                                alt={ item.title }
+                                loading="lazy"
+                                style={{ width: '100%' }}
+                            />
+                        </Link>
                     </ImageListItem>
-                ))}
+                )) }
             </ImageList>
-            <Grid container rowSpacing={2} sx={{mt: 5 }} columnSpacing={{ xs: 0, sm: 1, md: 3 }}>
+            <Grid container rowSpacing={ 2 } sx={{ mt: `${imageListHeight}px` }} columnSpacing={ { xs: 0, sm: 1, md: 3 }}>
                 <Grid xs={12} sm={12} md={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                     <Link href={ PATHS.CHATS + profileId } style={{ textDecoration: 'none', color: "#fff" }}>
                         <Button variant="contained" endIcon={<ChatBubbleOutlineIcon />}>
@@ -87,53 +127,28 @@ const itemData = [
     {
         img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
         title: 'Burger',
+        rows: 1,
+        cols: 1,
     },
     {
         img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
         title: 'Camera',
+        rows: 1,
+        cols: 1,
     },
     {
-        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-        title: 'Coffee',
-        cols: 2,
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+        rows: 1,
+        cols: 1,
     },
     {
-        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-        title: 'Hats',
-        cols: 2,
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+        rows: 1,
+        cols: 1,
     },
-    {
-        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-        title: 'Honey',
-        author: '@arwinneil',
-        rows: 3,
-        cols: 2,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-        title: 'Basketball',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-        title: 'Fern',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
-        rows: 2,
-        cols: 2,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
-        cols: 2,
-    },
+
+
+
 ];
